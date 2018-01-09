66cab519690efc6df5a2c6122d6b9e1fe07913f8
Hafsay <35266741+Hafsay@users.noreply.github.com>
2018-01-09 09:50:46 -0700

    added walmart changes

---
+++ CSharp Code Review/Code Review/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use the Salutation class fora walmart Greeter.
+            Salutation oldGuy = new Salutation("Welcome to Walmart!", "Thanks for shopping at Walmart!");
+            //Get the guy to talk.....
+            Console.WriteLine(oldGuy.Greet());
+            Console.WriteLine(oldGuy.SayFarewell());
+
+        }
+    }
+}
